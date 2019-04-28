43cdf23a3adc6b91837f85d1490ecb5c12f23f89
Stefan Lankes <slankes@eonerc.rwth-aachen.de>
2019-04-28 17:32:06 +0200

    add basic thread support for HermitCore

--- src/libstd/sys/hermit/thread.rs
+++ src/libstd/sys/hermit/thread.rs
@@ -1,35 +1,69 @@
+#![allow(dead_code)]
+
 use crate::ffi::CStr;
 use crate::io;
-use crate::sys::{unsupported, Void};
 use crate::time::Duration;
-use hermit::syscalls::{sys_usleep,sys_yield};
+use crate::mem;
+use core::u32;
+use hermit::syscalls::{Tid,sys_usleep,sys_yield,sys_spawn};
+use hermit::scheduler::task::{Priority,NORMAL_PRIO};
 
-pub struct Thread(Void);
+use crate::sys_common::thread::*;
 
-pub const DEFAULT_MIN_STACK_SIZE: usize = 4096;
+pub struct Thread {
+    tid: Tid
+}
+
+pub const DEFAULT_MIN_STACK_SIZE: usize = hermit::DEFAULT_STACK_SIZE;
 
 impl Thread {
-    // unsafe: see thread::Builder::spawn_unchecked for safety requirements
-    pub unsafe fn new(_stack: usize, _p: Box<dyn FnOnce()>)
+    pub unsafe fn new(_stack: usize, p: Box<dyn FnOnce()>)
         -> io::Result<Thread>
     {
-        unsupported()
+        let mut tid: Tid = u32::MAX;
+        let ret = sys_spawn(&mut tid as *mut Tid, thread_start, &*p as *const _ as *const u8 as usize,
+                            Priority::into(NORMAL_PRIO), 0);
+
+        return if ret == 0 {
+            Ok(Thread { tid: tid })
+        } else {
+            Err(io::Error::new(io::ErrorKind::Other, "Unable to create thread!"))
+        };
+
+        extern fn thread_start(main: usize) {
+            unsafe {
+                start_thread(main as *mut u8);
+            }
+        }
     }
 
+    #[inline]
     pub fn yield_now() {
         sys_yield();
     }
 
+    #[inline]
     pub fn set_name(_name: &CStr) {
         // nope
     }
 
+    #[inline]
     pub fn sleep(dur: Duration) {
         sys_usleep(dur.as_micros() as u64);
     }
 
     pub fn join(self) {
-        match self.0 {}
+        //match self.0 {}
+    }
+
+    #[inline]
+    pub fn id(&self) -> Tid { self.tid }
+
+    #[inline]
+    pub fn into_id(self) -> Tid {
+        let id = self.tid;
+        mem::forget(self);
+        id
     }
 }
 
