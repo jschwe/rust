15657ee0cb1d6a8c52271d380da34501e92591f3
Stefan Lankes <slankes@eonerc.rwth-aachen.de>
2019-05-30 22:01:08 +0000

    add the possibility to manage environment variables

--- src/libstd/sys/hermit/os.rs
+++ src/libstd/sys/hermit/os.rs
@@ -5,6 +5,16 @@ use crate::io;
 use crate::path::{self, PathBuf};
 use crate::str;
 use crate::sys::{unsupported, Void};
+use crate::collections::HashMap;
+use crate::vec;
+use crate::sync::Mutex;
+use crate::sync::atomic::{AtomicUsize, Ordering};
+use crate::sync::Once;
+
+extern "C" {
+    fn sys_getpid() -> u32;
+    fn sys_exit(arg: i32) ->!;
+}
 
 pub fn errno() -> i32 {
     0
@@ -60,32 +70,52 @@ pub fn current_exe() -> io::Result<PathBuf> {
     unsupported()
 }
 
-pub struct Env(Void);
+static ENV: AtomicUsize = AtomicUsize::new(0);
+static ENV_INIT: Once = Once::new();
 
-impl Iterator for Env {
-    type Item = (OsString, OsString);
-    fn next(&mut self) -> Option<(OsString, OsString)> {
-        match self.0 {}
+type EnvStore = Mutex<HashMap<OsString, OsString>>;
+
+fn get_env_store() -> Option<&'static EnvStore> {
+    unsafe { (ENV.load(Ordering::Relaxed) as *const EnvStore).as_ref() }
+}
+
+fn create_env_store() -> &'static EnvStore {
+    ENV_INIT.call_once(|| {
+        ENV.store(Box::into_raw(Box::new(EnvStore::default())) as _, Ordering::Relaxed)
+    });
+    unsafe {
+        &*(ENV.load(Ordering::Relaxed) as *const EnvStore)
     }
 }
 
+pub type Env = vec::IntoIter<(OsString, OsString)>;
+
 pub fn env() -> Env {
-    panic!("not supported on HermitCore")
+    let clone_to_vec = |map: &HashMap<OsString, OsString>| -> Vec<_> {
+        map.iter().map(|(k, v)| (k.clone(), v.clone()) ).collect()
+    };
+
+    get_env_store()
+        .map(|env| clone_to_vec(&env.lock().unwrap()) )
+        .unwrap_or_default()
+        .into_iter()
 }
 
-pub fn getenv(_k: &OsStr) -> io::Result<Option<OsString>> {
-    //Ok(GetEnvSysCall::perform(k))
-    Ok(None)
+pub fn getenv(k: &OsStr) -> io::Result<Option<OsString>> {
+    Ok(get_env_store().and_then(|s| s.lock().unwrap().get(k).cloned() ))
 }
 
-pub fn setenv(_k: &OsStr, _v: &OsStr) -> io::Result<()> {
-    //Ok(SetEnvSysCall::perform(k, Some(v)))
+pub fn setenv(k: &OsStr, v: &OsStr) -> io::Result<()> {
+    let (k, v) = (k.to_owned(), v.to_owned());
+    create_env_store().lock().unwrap().insert(k, v);
     Ok(())
 }
 
-pub fn unsetenv(_k: &OsStr) -> io::Result<()> {
-   // Ok(SetEnvSysCall::perform(k, None))
-   Ok(())
+pub fn unsetenv(k: &OsStr) -> io::Result<()> {
+    if let Some(env) = get_env_store() {
+        env.lock().unwrap().remove(k);
+    }
+    Ok(())
 }
 
 pub fn temp_dir() -> PathBuf {
@@ -96,10 +126,14 @@ pub fn home_dir() -> Option<PathBuf> {
     None
 }
 
-pub fn exit(_code: i32) -> ! {
-    loop {}
+pub fn exit(code: i32) -> ! {
+    unsafe {
+        sys_exit(code);
+    }
 }
 
 pub fn getpid() -> u32 {
-    panic!("no pids on hermit")
+    unsafe {
+        sys_getpid()
+    }
 }
