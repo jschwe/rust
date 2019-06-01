8f01b24c322b1fba644352cb363dd8f3807a9b2e
Stefan Lankes <slankes@eonerc.rwth-aachen.de>
2019-06-01 07:08:31 +0000

    use mutex as rwlock (just a workaround)

--- src/libstd/sys/hermit/rwlock.rs
+++ src/libstd/sys/hermit/rwlock.rs
@@ -1,72 +1,51 @@
-use crate::cell::UnsafeCell;
+use super::mutex::Mutex;
 
 pub struct RWLock {
-    mode: UnsafeCell<isize>,
+    mutex: Mutex
 }
 
 unsafe impl Send for RWLock {}
-unsafe impl Sync for RWLock {} // no threads on wasm
+unsafe impl Sync for RWLock {}
 
 impl RWLock {
     pub const fn new() -> RWLock {
         RWLock {
-            mode: UnsafeCell::new(0),
+            mutex: Mutex::new()
         }
     }
 
     #[inline]
     pub unsafe fn read(&self) {
-        let mode = self.mode.get();
-        if *mode >= 0 {
-            *mode += 1;
-        } else {
-            rtabort!("rwlock locked for writing");
-        }
+        self.mutex.lock();
     }
 
     #[inline]
     pub unsafe fn try_read(&self) -> bool {
-        let mode = self.mode.get();
-        if *mode >= 0 {
-            *mode += 1;
-            true
-        } else {
-            false
-        }
+        self.mutex.try_lock()
     }
 
     #[inline]
     pub unsafe fn write(&self) {
-        let mode = self.mode.get();
-        if *mode == 0 {
-            *mode = -1;
-        } else {
-            rtabort!("rwlock locked for reading")
-        }
+        self.mutex.lock();
     }
 
     #[inline]
     pub unsafe fn try_write(&self) -> bool {
-        let mode = self.mode.get();
-        if *mode == 0 {
-            *mode = -1;
-            true
-        } else {
-            false
-        }
+        self.mutex.try_lock()
     }
 
     #[inline]
     pub unsafe fn read_unlock(&self) {
-        *self.mode.get() -= 1;
+        self.mutex.unlock();
     }
 
     #[inline]
     pub unsafe fn write_unlock(&self) {
-        *self.mode.get() += 1;
+        self.mutex.unlock();
     }
 
     #[inline]
     pub unsafe fn destroy(&self) {
+        self.mutex.destroy();
     }
 }
